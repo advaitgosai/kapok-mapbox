15eb1fe226fc235303b8172c4d4cda7b33d82ca1
Advait <advait.gosai@gmail.com>
2022-12-04 16:59:58 -0500

    Mapbox First Stages

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,9 @@
+import 'package:latlong2/latlong.dart';
+
+class AppConstants {
+  static const String mapBoxAccessToken = 'pk.eyJ1Ijoia2Fwb2stZGV2ZWxvcGVyIiwiYSI6ImNqbzFscjE2ejBjd2Mza210amdtN252OXYifQ.0gR_XnITpdJF-RquzFfIcQ';
+
+  static const String mapBoxStyleId = 'clb9w4s3d000i14ruohvky6uy';
+
+  static final myLocation = LatLng(51.5090214, -0.1982948);
+}
